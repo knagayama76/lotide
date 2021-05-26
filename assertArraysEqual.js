f43694ce46bf0eca0fbac534703299d0c749ce85
Kaoru Nagayama <kaoair@hotmail.com>
2021-05-26 13:49:37 -0400

    updated

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,26 @@
 "use strict";
 
-const assertArraysEqual = function (arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++)
-    for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] === arr2[j]) {
-        return true;
-      } else {
-        return false;
-      }
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected)
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+// };
+
+const eqArrays = function (values1, values2) {
+  if (values1.length !== values2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < values1.length; i++) {
+    if (values1[i] !== values2[i]) {
+      return false;
     }
+  }
+  return true;
 };
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+const assertArraysEqual = function (arr1, arr2) {
+  console.log(eqArrays(arr1, arr2));
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
