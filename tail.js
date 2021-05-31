63fa3b56cdbbce93e1a168a7f8d6f286c0b28351
Kaoru Nagayama <kaoair@hotmail.com>
2021-05-31 13:48:49 -0400

    tail refactor

--- tail.js
+++ tail.js
@@ -1,17 +1,9 @@
 "use strict";
 
-const assertEqual = function (actual, expected) {
-  if (actual === expected)
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const tail = function (arr) {
   let newArr = [];
   newArr = arr.shift();
   return newArr;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
