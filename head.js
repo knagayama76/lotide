83155b3f4a7473f5da965482645bcba1e2a248b1
knagayama76 <knagayama76>
2022-01-10 18:24:15 -0500

    added new function

---
+++ head.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const head = function (arr) {
+  return arr.shift();
+};
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected)
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
