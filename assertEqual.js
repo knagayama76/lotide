5aa4aea27a45b97485290dae5692a5c9ad9f83c4
knagayama76 <knagayama76>
2022-01-10 18:14:09 -0500

    create assertion function

--- assertEqual.js
+++ assertEqual.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected)
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// Comparing identical strings
+assertEqual("Bootcamp", "Bootcamp");
+// Comparing non-identical strings
+assertEqual("Lighthouse Labs", "Bootcamp");
+// Comparing identical numbers
+assertEqual(1, 1);
+// Comparing non-identical numbers
+assertEqual(1, 3);
