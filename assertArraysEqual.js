df29fab28adb1b35f14303aeaa41fb331e1f7bf7
knagayama76 <knagayama76>
2022-01-11 18:07:44 -0500

    function created

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected)
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// function(takes 2 arrays)
+const eqArrays = function (arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) return true;
+  }
+};
+
+// Imprelent assertArraysEqual which will take two arrays
+const assertArraysEqual = function (actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (eqArrays(actual, expected)) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  }
+};
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
