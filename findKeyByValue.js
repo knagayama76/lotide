79cb11468d19feba059a8637cb81a6b7eaca573d
knagayama76 <knagayama76>
2022-01-12 18:55:06 -0500

    findKeyByValue function implemented

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected)
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+const findKeyByValue = function (obj, value) {
+  const listOfKeys = Object.keys(obj);
+  for (const key of listOfKeys) {
+    if (bestTVShowsByGenre[key] === value) return key;
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
