0a3a538403fa5e03f7aa1cdb4a152603f385ee2b
knagayama76 <knagayama76>
2022-01-17 13:43:54 -0500

    add eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -14,7 +14,4 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
